80065bda842c053dc067af2a3a7130dabedd6321
Arman Mielke <arman.mielke@yahoo.de>
2020-05-10 00:25:01 -0400

    Adjust compare_hands() for community card

--- src/simple_poker/game_over_utils.cpp
+++ src/simple_poker/game_over_utils.cpp
@@ -18,8 +18,26 @@ bool game_over(const History& history) {
 /// Returns -1 if player has a weaker hand than opponent.
 /// Returns 0 if both hands are of equal strength.
 int8_t compare_hands(const Deck& cards, size_t player_card_index) {
-    size_t opponent_card_index = 1 - player_card_index;
-    if (cards[player_card_index] > cards[opponent_card_index]) {
+    Card player_card = cards[player_card_index];
+    Card opponent_card = cards[1 - player_card_index];
+    Card community_card = cards[2];
+
+    // player has a straight (if the opponent has a straight, too, then it must be one lower)
+    if (player_card == community_card + 1) { return 1; }
+    // opponent has a straight (if the player has a straight, too, then it must be one lower)
+    if (opponent_card == community_card + 1) { return -1; }
+    // player has a straight, opponent doesn't
+    if (player_card == community_card - 1) { return 1; }
+    // opponent has a straight, player doesn't
+    if (opponent_card == community_card - 1) { return -1; }
+
+    // draw (both player's high card is the community card)
+    if (player_card < community_card && opponent_card < community_card) {
+        return 0;
+    }
+
+    // the person with the higher card wins
+    if (player_card > opponent_card) {
         return 1;
     } else {
         return -1;
