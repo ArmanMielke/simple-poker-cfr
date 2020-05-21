dca95ef67267432c5b4078bd93d387d5d44a6ff7
Arman Mielke <arman.mielke@yahoo.de>
2020-05-20 23:53:42 -0400

    Add SimplePlayer

---
+++ src/simple_poker/simulation/simple_player.h
@@ -0,0 +1,16 @@
+#ifndef PYKER_SIMPLE_PLAYER_H
+#define PYKER_SIMPLE_PLAYER_H
+
+
+#include "simple_poker/information_set.h"
+#include "simple_poker/constants.h"
+
+
+/// Abstract base class for objects that control the behaviour of a player for simplified poker.
+class SimplePlayer {
+public:
+    [[nodiscard]] virtual Action do_betting_action(InformationSet info_set) = 0;
+};
+
+
+#endif //PYKER_SIMPLE_PLAYER_H
